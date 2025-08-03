d28988fbb1240f6081852b1ac61ba7a0d088f997
Rodrigo Lima <rodrigotiagocl@gmail.com>
2025-08-03 03:17:54 -0300

    feat: Add generic pagination, sorting, and searching infrastructure
    Created a reusable infrastructure for handling list queries.
    
    - Added QueryParameters class to define client-side query options.
    - Added PagedResult<T> class to provide a standardized paginated response envelope.
    - Implemented QueryableExtensions for applying sorting and pagination to any IQueryable source.
    - Added System.Linq.Dynamic.Core for dynamic sorting capabilities.

---
+++ myCharacter_Api/Helpers/QueryParametes.cs
@@ -0,0 +1,17 @@
+using Microsoft.IdentityModel.Tokens;
+
+public class QueryParameters
+{
+    public int PageNumber { get; set; } = 1;
+    private const int MaxPageSize = 100;
+    private int _pageSize = 10;
+    public int PageSize
+    {
+        get => _pageSize;
+        set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+    }
+
+    public string? SortBy { get; set; }
+    public string? SortOrder { get; set; }
+    public string? SearchTerm { get; set; }
+}
